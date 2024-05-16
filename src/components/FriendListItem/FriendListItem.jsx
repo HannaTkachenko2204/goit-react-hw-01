8089a96be24ae583cf6ca7df24a255604ee129bd
Анна <anna.kuvakina91@gmail.com>
2024-05-16 14:01:39 +0300

    prett

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,16 @@
-import clsx from 'clsx'
-import css from './FriendListItem.module.css'
+import clsx from "clsx";
+import css from "./FriendListItem.module.css";
 
-const FriendListItem = ({avatar, name, isOnline}) => {
-  const classNames = clsx(
-		isOnline ? css.online : css.offline
-	)
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  const classNames = clsx(isOnline ? css.online : css.offline);
 
-    return (
-        <div>
-        <img src={avatar} alt={name} width="48" />
-        <p>{name}</p>
-        <p className={classNames}>{isOnline ? 'Online': 'Offline'}</p>
-      </div> 
-    )
-}
+  return (
+    <div>
+      <img src={avatar} alt={name} width="48" />
+      <p>{name}</p>
+      <p className={classNames}>{isOnline ? "Online" : "Offline"}</p>
+    </div>
+  );
+};
 
-export default FriendListItem
+export default FriendListItem;
